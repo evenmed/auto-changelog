d00f6c32f99e1c00291dde1cea1630e1a17bc043
evenmed <emilio@circular.co>
2022-07-19 18:18:06 -0500

    🚨 New step

--- changelog.sh
+++ changelog.sh
@@ -181,6 +181,9 @@ done
 # Finally, add it all into the changelog!
 echo -e "$STRING_TO_ADD\n$CHANGELOG_CONTENT" > dChangelog.md
 
+echo ::set-output name=status::"updated"
+echo ::set-output name=text::"$STRING_TO_ADD"
+
 # Push the changes into the repo
 git config user.name "evenmed"
 git config user.email "emilio@circular.co"
